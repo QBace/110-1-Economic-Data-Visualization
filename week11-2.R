9e1e9568cb41b2ca03879c4f866797da3107da4a
QBace <91184212+QBace@users.noreply.github.com>
2021-11-30 22:27:05 +0800

    Update week11-2.R

--- week11-2.R
+++ week11-2.R
@@ -1,28 +1,36 @@
-library(reshape)
+library(readr)
+library(reshape2)
 library(ggplot2)
-data3 <-data.frame("Year"=c(89:109),
-                   "抽蓄水力"<-c(8.78,8.63,8.15,7.82,7.52,7.20,6.96,6.83,6.74,6.47,6.36,6.28,6.35,6.32,6.38,6.34,6.18,6.21,5.83,5.45,5.23),
-                   "再生能源"<-c(6.14,6.05,5.99,5.74,5.53,5.34,5.37,5.52,5.65,5.73,5.99,7.80,8.31,8.73,9.25,9.80,10.26,11.43,13.16,14.97,18.48),
-                   "燃油"<-c(18.24,15.39,11.21,10.70,10.18,9.99,9.66,9.48,9.34,8.97,8.86,8.03,8.12,8.08,8.15,8.10,7.89,6.14,5.76,5.38,3.20),
-                   "核能"<-c(17.36,17.07,16.12,15.45,14.87,14.24,13.76,13.51,13.31,12.78,12.57,12.42,12.55,12.49,12.61,12.54,12.21,12.28,10.10,8.10,7.78),
-                   "燃氣"<-c(16.08,20.01,23.44,26.65,29.54,30.71,31.34,33.42,34.16,36.49,37.14,36.72,37.10,36.95,37.38,37.15,36.18,34.58,35.80,35.34,35.78),
-                   "燃煤"<-c(33.41,32.85,35.08,33.64,32.36,32.52,32.91,31.24,30.79,29.56,29.08,28.74,27.57,27.43,26.23,26.07,27.29,29.36,29.35,30.76,29.53)
-)
-data4 <- melt(data3,id.var ="Year")
-plot(ggplot(data4, aes(x = Year, y = value, fill = variable)) +
-       geom_area(stat ="identity")+
-       labs( x="民國",y = "佔比",size=10)+
-       geom_hline(yintercept =25,color="red",linetype=5,size=0.3)+
-       geom_hline(yintercept =50,color="red",linetype=5,size=0.3)+
-       geom_hline(yintercept =75,color="red",linetype=5,size=0.3))+
-  scale_y_continuous (expand = c(0,0))+
-  scale_x_continuous (expand = c(0,0))+
-  labs( title = "歷年台電系統發電廠裝置容量表",subtitle = "Structure of Electricity Installed Capacity")+
-  scale_fill_manual(values =c("#5eb7b6","#73a74d","#c2604a","#e1872e","#57a9d5","#999b9f"),names(NULL),
+歷年台電系統發電廠裝置容量表 <- read_csv("~/Desktop/歷年台電系統發電廠裝置容量表.csv",col_types = cols(year = col_integer()))
+select1<-select(歷年台電系統發電廠裝置容量表,1:7) #圖取資料範圍
+
+data5 <- melt(select1,id.vars= c("year")) #melt處理資料
+ggplot(data5, aes(x = year, y = value, fill = variable)) + #依照x(year)做排序,固定抽蓄水力、燃煤、燃油、燃氣、再生能源、核能為variable,其餘值為value,
+   geom_area(stat ="identity")+ #繪製面積圖
+   geom_text(aes(x=2001,y=8,label="17.36%"),size=3.2,color="white")+ #左右文字
+   geom_text(aes(x=2001,y=20,label="6.14%"),size=3.2,color="white")+
+   geom_text(aes(x=2001,y=30,label="16.08%"),size=3.2,color="white")+
+   geom_text(aes(x=2001,y=50,label="18.24%"),size=3.2,color="white")+
+   geom_text(aes(x=2001,y=75,label="33.41%"),size=3.2,color="white")+
+   geom_text(aes(x=2000.8,y=96,label="90%"),size=3.2,color="white")+
+   geom_text(aes(x=2019,y=5,label="7.78%"),size=3.2,color="white")+
+   geom_text(aes(x=2019,y=16,label="18.48%"),size=3.2,color="white")+
+   geom_text(aes(x=2019,y=40,label="35.78%"),size=3.2,color="white")+
+   geom_text(aes(x=2019.2,y=62,label="3.2%"),size=3.2,color="white")+
+   geom_text(aes(x=2019,y=80,label="29.53%"),size=3.2,color="white")+
+   geom_text(aes(x=2019,y=97,label="5.23%"),size=3.2,color="white")+
+   geom_hline(yintercept =25,color="red",linetype=5,size=0.3,alpha=0.3)+
+   geom_hline(yintercept =50,color="red",linetype=5,size=0.3,alpha=0.3)+
+   geom_hline(yintercept =75,color="red",linetype=5,size=0.3,alpha=0.3)+
+  scale_y_continuous ("佔比",expand = c(0,0))+ #y軸美學
+  scale_x_continuous ("西元",expand = c(0,0))+ #x軸美學
+  labs( title = "歷年台電系統發電廠裝置容量表",subtitle = "Structure of Electricity Installed Capacity")+ #標題與副標題
+  scale_fill_manual(values =c("#57a9d5","#999b9f","#c2604a","#5eb7b6","#73a74d","#e1872e"), #圖示顏色
+                    names(NULL), #圖示標題取消null
                     labels=c("抽蓄水力
-Pumped Hydro","再生能源
-Renewable Energy","燃油
-Oil-Fired","核能
-Nuclear","燃氣
-LNG-Fired","燃煤
-Coal-Fired"))
+Pumped Hydro","燃煤
+Coal-Fired","燃油
+Oil-Fired","燃氣
+LNG-Fired","再生能源
+Renewable Energy","核能
+Nuclear")) #圖示文字
