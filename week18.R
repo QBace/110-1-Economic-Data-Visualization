b3b8bb6f683b007054d8e7c56cea57c731df2134
QBace <91184212+QBace@users.noreply.github.com>
2022-01-19 12:20:32 +0800

    Update week18.R

--- week18.R
+++ week18.R
@@ -18,59 +18,35 @@ background$台灣本島$terrain <-
   function(){
     ggmap::ggmap(taiwan_stamen$terrain)
   }
-
 econDV2::Object(singal)
-singal$data <- read_csv("~/Desktop/行動通信業務基地臺統計.csv")
-singal$data |>
-  group_by(鄉鎮區)|>
-  summarise(
-    基地臺,
-    基地臺2=cut(基地臺, c(0, 300,800, 2000))
-  )|>
-  ungroup()-> singal$data_frequency
-left_data <-
-  sf_taiwan_simplified$`台灣本島`$鄉鎮區
-right_data <- 
-  singal$data_frequency |>
-  mutate(
-    鄉鎮區=
-      stringr::str_replace(鄉鎮區, '台', "臺"))
-
-singal$join$left <- {
-  left_data |> 
-    dplyr::left_join(
-      right_data, by=c("map_id"="鄉鎮區")
-    )
-}
-
-singal$join$right <- {
-  left_data |> 
-    dplyr::right_join(
-      right_data, by=c("map_id"="鄉鎮區")
-    )
-}
-singal$join$inner <- {
-  left_data |> 
+singal$data <- read_excel("~/Desktop/行動通信業務基地臺統計1.xlsx")
+singal$inner <- {
+  sf_taiwan_simplified$台灣本島$鄉鎮區 |>
     dplyr::inner_join(
-      right_data, by=c("map_id"="鄉鎮區")
+      singal$data, by=c("map_id"="鄉鎮區")
     )
 }
-singal$sf <- singal$join$inner
+singal$cut_inner<-singal$inner
+singal$cut_inner$基地臺 |> cut(c(0,200,500,1200,2000),ordered_result = T) -> .cut
+levels(.cut) <- c("1-200","200-500","500-1200","1200-2000")
+singal$cut_inner$基地臺 <- .cut
+abc<-function(){
 econDV2::ggmap2(taiwan_stamen$terrain)+
   econDV2::geom_sf_overggmap(
-    data=singal$sf,
+    data=singal$cut_inner,
     mapping=aes(
       fill=基地臺
     ),
     color="white",
     size=0.15,
-    alpha=0.8,
+    alpha=0.6,
     inherit.aes = F
   ) +
-  colorspace::scale_fill_continuous_sequential(
-    palette="blue", 
-    na.value="#dbd7a8"
-  )+
+colorspace::scale_fill_discrete_sequential(
+  palette="blue", 
+  na.value="#dbd7a8"
+)+
+  theme_void()+
   labs( title = "台灣110年行動通信業務基地臺統計",caption = "資料來源：國家通訊傳播委員會\n行動通信業務基地臺統計(區分鄉鎮)")
-
+}
 #plotly::ggplotly(abc)|> htmltools::save_html(file="myplotly.html")
